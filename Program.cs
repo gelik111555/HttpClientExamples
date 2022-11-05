45cb609a8eb54a75a92541f54199995234cede1a
gelik111555 <gelik111555@gmail.com>
2022-11-05 13:16:54 +0300

    feat: Запрос к удалённом серверу для получения данных

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    private const string dataUrl = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+    private static void Main(string[] args)
+    {
+        var client = new HttpClient();
+
+        var result = client.GetAsync(dataUrl).Result;
+        var csvStr = result.Content.ReadAsStringAsync().Result;
+        Console.WriteLine(csvStr);
+    }
+}
