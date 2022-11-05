ce27dc4351f996825ca10df7ae5da8be5a91742a
gelik111555 <gelik111555@gmail.com>
2022-11-05 13:55:06 +0300

    feat: Таким образом мы скачиваем только первую строку с сервера, и получаем только нужны нам данные в дальнейшем

--- Program.cs
+++ Program.cs
@@ -1,13 +1,59 @@
-﻿class Program
+﻿using System.Data;
+using System.Globalization;
+
+class Program
 {
     private const string dataUrl = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
-    private static void Main(string[] args)
+    /// <summary>
+    /// Метод будет возвращать поток для получения текстовых данных
+    /// </summary>
+    /// <returns>вернёт поток из которого мы сможем читать данные</returns>
+    private static async Task<Stream> GetDataStream()
     {
         var client = new HttpClient();
+        // тут будет скачен только заголовок ответа, а тело ответа будет не принятым,
+        // останется в буфере сетевой карты
+        var response = await client.GetAsync(dataUrl, HttpCompletionOption.ResponseHeadersRead);
+        return await response.Content.ReadAsStreamAsync();
+    }
+    /// <summary>
+    ///Построчное чтение данных из буфера с возможностью прервать чтение в любой момент
+    ///без необходимости скачивать польностью все данные
+    /// </summary>
+    /// <returns></returns>
+    private static  IEnumerable<string> GetDataLines()
+    {
+        // запрос к серверу
+         using var dataStream =  GetDataStream().Result;
+        // обеспечит построчное чтение из потока
+        using var dataReader = new StreamReader(dataStream);
+        // тут будет считана одна строка и возвращена как результат
+        while (!dataReader.EndOfStream)
+        {
+            var line =  dataReader.ReadLineAsync().Result;
+            if (string.IsNullOrWhiteSpace(line)) continue;
+            yield return line;
+        }
+    }
 
-        var result = client.GetAsync(dataUrl).Result;
-        var csvStr = result.Content.ReadAsStringAsync().Result;
-        Console.WriteLine(csvStr);
+    private static DateTime[] GetDates() => GetDataLines()
+        .First()
+        .Split(',')
+        .Skip(4)
+        .Select(x => DateTime.Parse(x, CultureInfo.InvariantCulture))
+        .ToArray();
+    private static void Main(string[] args)
+    {
+        //var client = new HttpClient();
+
+        //var result = client.GetAsync(dataUrl).Result;
+        //var csvStr = result.Content.ReadAsStringAsync().Result;
+        //foreach (var item in GetDataLines())
+        //{
+        //    Console.WriteLine(item);
+        //}
+        var dates = GetDates();
+        Console.WriteLine(string.Join("\r\n ", dates));
     }
 }
